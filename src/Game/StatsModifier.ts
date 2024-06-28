xivintheshell/xivintheshell

commit a84b246043de807d024d546c2c87d371552c69f2

Dawntrail - Change Mana Mana

src/Game/StatsModifier.ts
@@ -61,20 +61,12 @@ export class StatsModifier
 		// umbral ice
 		let ui = resources.get(ResourceType.UmbralIce);
 		let uiMod = new StatsModifier();
-		if (ui.availableAmount() === 1) {
-			uiMod.manaRegen = 3000;
-			uiMod.manaCostFire = 0;
-			uiMod.manaCostIce = 0.75;
-		} else if (ui.availableAmount() === 2) {
-			uiMod.manaRegen = 4500;
-			uiMod.manaCostFire = 0;
-			uiMod.manaCostIce = 0.5;
-		} else if (ui.availableAmount() === 3) {
-			uiMod.manaRegen = 6000;
+		if (ui.availableAmount() > 0) {
 			uiMod.manaCostFire = 0;
 			uiMod.manaCostIce = 0;
-			uiMod.castTimeFire = 0.5;
-		}
+			if (ui.availableAmount() === 3)
+				uiMod.castTimeFire = 0;
+		} 
 		modifiers.push(uiMod);
 
 		// astral fire & umbral hearts

src/Game/GameState.ts
@@ -331,6 +331,17 @@ export class GameState {
 		}
 	}
 
+	gainUmbralMana() {
+		let mpToGain = 0;
+		switch(this.resources.get(ResourceType.UmbralIce).availableAmount()) {
+			case 1: mpToGain = 2500;  break;
+			case 2: mpToGain = 5000;  break;
+			case 3: mpToGain = 10000; break;
+			default: mpToGain = 0; break;
+		}
+		this.resources.get(ResourceType.Mana).gain(mpToGain);
+	}
+
 	captureManaCostAndUHConsumption(aspect: Aspect, baseManaCost: number) {
 		let mod = StatsModifier.fromResourceState(this.resources);
 

src/Game/Skills.ts
@@ -195,6 +195,7 @@ export class SkillsList extends Map<SkillName, Skill> {
 				if (game.getFireStacks() === 0) // no AF
 				{
 					game.castSpell({skillName: SkillName.Blizzard, onCapture: (cap: SkillCaptureCallbackInfo) => {
+						game.gainUmbralMana();
 						game.resources.get(ResourceType.UmbralIce).gain(1);
 						game.startOrRefreshEnochian();
 					}, onApplication: (app: SkillApplicationCallbackInfo) => {
@@ -434,6 +435,7 @@ export class SkillsList extends Map<SkillName, Skill> {
 			},
 			(game, node) => {
 				game.castSpell({skillName: SkillName.Blizzard3, onCapture: (cap: SkillCaptureCallbackInfo) => {
+					game.gainUmbralMana();
 					game.switchToAForUI(ResourceType.UmbralIce, 3);
 					game.startOrRefreshEnochian();
 				}, onApplication: (app: SkillApplicationCallbackInfo) => {
@@ -448,6 +450,7 @@ export class SkillsList extends Map<SkillName, Skill> {
 			},
 			(game, node) => {
 				game.castSpell({skillName: SkillName.Freeze, onCapture: (cap: SkillCaptureCallbackInfo) => {
+					game.gainUmbralMana();
 					game.resources.get(ResourceType.UmbralHeart).gain(3);
 				}, onApplication: (app: SkillApplicationCallbackInfo) => {
 				}, node: node});
@@ -484,6 +487,7 @@ export class SkillsList extends Map<SkillName, Skill> {
 			},
 			(game, node) => {
 				game.castSpell({skillName: SkillName.Blizzard4, onCapture: (cap: SkillCaptureCallbackInfo) => {
+					game.gainUmbralMana();
 					game.resources.get(ResourceType.UmbralHeart).gain(3);
 				}, onApplication: (app: SkillApplicationCallbackInfo) => {
 				}, node: node});
@@ -607,6 +611,7 @@ export class SkillsList extends Map<SkillName, Skill> {
 				game.useInstantSkill({
 					skillName: SkillName.UmbralSoul,
 					onCapture: () => {
+						game.gainUmbralMana();
 						game.resources.get(ResourceType.UmbralIce).gain(1);
 						game.resources.get(ResourceType.UmbralHeart).gain(1);
 						game.startOrRefreshEnochian();

commit 8a685379addfe92124eff2dccef23e2e904c9e8d

Dawntrail - Fix Fire 3 Hard Cast
* Broke it in a84b246

src/Game/StatsModifier.ts
@@ -65,7 +65,7 @@ export class StatsModifier
 			uiMod.manaCostFire = 0;
 			uiMod.manaCostIce = 0;
 			if (ui.availableAmount() === 3)
-				uiMod.castTimeFire = 0;
+				uiMod.castTimeFire = 0.5;
 		} 
 		modifiers.push(uiMod);
 