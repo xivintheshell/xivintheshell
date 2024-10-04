9125ac94ce7da5bc605ee4e6aced4cf1688bc12f
miyehn <rainduym@gmail.com>
2024-10-04 00:24:34 -0700

    fix deps array causing update loop (?)

--- src/Components/Common.tsx
+++ src/Components/Common.tsx
@@ -461,7 +461,9 @@ export function Checkbox(props: {
 		}
 		setChecked(defaultChecked);
 		props.onChange(defaultChecked);
-	}, [props]);
+		// myn: props really shouldn't update so should be fine to not have them in deps array..
+		// eslint-disable-next-line
+	}, []);
 	const checkboxStyle: CSSProperties = {
 		position: "relative",
 		top: 3,
