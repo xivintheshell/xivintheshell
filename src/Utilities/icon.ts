c4b541634574ff8f2e981363faa9b21cf174af04
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-11-10 14:25:14 -0600

    Coding style fix

--- src/Utilities/icon.ts
+++ src/Utilities/icon.ts
@@ -1,6 +1,6 @@
-export function iconUrl(icon: number, stacks?: number): string {
+export function iconUrl(icon: number, stacks: number = 1): string {
 	const group = Math.floor(icon / 1000) * 1000
-    const iconNumberStacked = icon + (stacks ?? 1) - 1
+    const iconNumberStacked = icon + stacks - 1 // Normalize 1 stack and stack-less buffs back to the raw icon number
 	const gamePath = `ui/icon/${group.toString(10).padStart(6, '0')}/${iconNumberStacked.toString(10).padStart(6, '0')}_hr1.tex`
 	return `https://beta.xivapi.com/api/1/asset/${gamePath}?format=png`
 }
