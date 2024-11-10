c06a5125491b2c0bd0cef8331877f157f4eec2bc
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-11-10 13:48:27 -0600

    Update role actions to use xivapi icon URLS instead of local assets

--- src/Game/Jobs/RoleActions.ts
+++ src/Game/Jobs/RoleActions.ts
@@ -3,6 +3,7 @@ import {SkillName, ResourceType} from "../Common";
 import {makeResourceAbility} from "../Skills";
 import {DoTBuff, EventTag} from "../Resources"
 import {Traits, TraitName} from "../Traits";
+import { iconUrl } from "../../Utilities/icon";
 
 const CASTER_JOBS = [ShellJob.BLM, ShellJob.PCT];
 
@@ -11,21 +12,21 @@ makeResourceAbility(CASTER_JOBS, SkillName.Addle, 8, ResourceType.cd_Addle, {
 	applicationDelay: 0.621, // delayed
 	cooldown: 90,
 	duration: (state) => (Traits.hasUnlocked(TraitName.EnhancedAddle, state.config.level) && 15) || 10,
-	assetPath: "CasterRole/Addle.png",
+	assetPath: iconUrl(861),
 });
 
 makeResourceAbility(CASTER_JOBS, SkillName.Swiftcast, 18, ResourceType.cd_Swiftcast, {
 	rscType: ResourceType.Swiftcast,
 	applicationDelay: 0, // instant
 	cooldown: 40, // set by trait in constructor
-	assetPath: "CasterRole/Swiftcast.png",
+	assetPath: iconUrl(866),
 });
 
 makeResourceAbility(CASTER_JOBS, SkillName.LucidDreaming, 14, ResourceType.cd_LucidDreaming, {
 	rscType: ResourceType.LucidDreaming,
 	applicationDelay: 0.623, // delayed
 	cooldown: 60,
-	assetPath: "CasterRole/Lucid Dreaming.png",
+	assetPath: iconUrl(865),
 	onApplication: (state, node) => {
 		let lucid = state.resources.get(ResourceType.LucidDreaming) as DoTBuff;
 		lucid.node = node;
@@ -41,19 +42,19 @@ makeResourceAbility(CASTER_JOBS, SkillName.Surecast, 44, ResourceType.cd_Surecas
 	rscType: ResourceType.Surecast,
 	applicationDelay: 0, // surprisingly instant because arms length is not
 	cooldown: 120,
-	assetPath: "CasterRole/Surecast.png",
+	assetPath: iconUrl(869),
 });
 
 makeResourceAbility(CASTER_JOBS, SkillName.Tincture, 1, ResourceType.cd_Tincture, {
 	rscType: ResourceType.Tincture,
 	applicationDelay: 0.64, // delayed // somewhere in the midrange of what's seen in logs
 	cooldown: 270,
-	assetPath: "CasterRole/Tincture.png",
+	assetPath: iconUrl(20704),
 });
 
 makeResourceAbility(CASTER_JOBS, SkillName.Sprint, 1, ResourceType.cd_Sprint, {
 	rscType: ResourceType.Sprint,
 	applicationDelay: 0.133, // delayed
 	cooldown: 60,
-	assetPath: "General/Sprint.png",
+	assetPath: iconUrl(104),
 });
