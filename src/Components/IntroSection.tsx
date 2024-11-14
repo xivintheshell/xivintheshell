43647b868e448b8e261dc5e55ef0b1ee49c4a501
miyehn <rainduym@gmail.com>
2024-11-13 23:25:41 -0800

    remove debug and implementation notes sections

--- src/Components/IntroSection.tsx
+++ src/Components/IntroSection.tsx
@@ -1,8 +1,7 @@
 import React, {CSSProperties} from 'react';
-import {ShellJob} from "../Controller/Common";
+import {clearCachedValues, ShellJob} from "../Controller/Common";
 import {Expandable, Help, ButtonIndicator} from "./Common";
 import {localize} from "./Localization";
-import {DebugOptions} from "./DebugOptions";
 import changelog from "../changelog.json"
 import {getCurrentThemeColors} from "./ColorTheme";
 
@@ -174,47 +173,72 @@ export function IntroSection(props: {
 				})}
 				<div className="paragraph" style={{marginTop: 16}}>
 					<Expandable title={"Troubleshoot"} titleNode={localize({en: <b>troubleshoot</b>, zh: <b>常见问题</b>, ja: <b>トラブルシューティング</b>})} content={
-						localize({
-						en: <div>
-								<div className="paragraph">
-									I can't guarantee that my updates are always backward compatible. If your fight record files aren't loading properly but you don't understand why, contact me and I'll try my best to help.
-								</div>
-								<div className="paragraph">
-								If the browser cache is somehow messed up (likely due to invalid game states), this is how to reset it:<br/>
-								Enter this tool from <b>{THIS_DOMAIN + "/#/{command}"}</b> replacing <b>{"{command}"}</b> with one of the following:
-								<ul>
-									<li style={smallGap}><b>resetResourceOverrides</b>: delete all resource overrides and all actions on the current timeline.</li>
-									<li style={smallGap}><b>resetAll</b>: delete all browser-cached settings.</li>
-								</ul>
+						<>
+							{localize({
+								en: <div>
+									<div className="paragraph">
+										I can't guarantee that my updates are always backward compatible. If your fight
+										record files aren't loading properly but you don't understand why, contact me
+										and I'll try my best to help.
+									</div>
+									<div className="paragraph">
+										If the browser cache is somehow messed up (likely due to invalid game states),
+										this is how to reset it:<br/>
+										Enter this tool
+										from <b>{THIS_DOMAIN + "/#/{command}"}</b> replacing <b>{"{command}"}</b> with
+										one of the following:
+										<ul>
+											<li style={smallGap}><b>resetResourceOverrides</b>: delete all resource
+												overrides and all actions on the current timeline.
+											</li>
+											<li style={smallGap}><b>resetAll</b>: delete all browser-cached settings.
+											</li>
+										</ul>
+									</div>
+								</div>,
+								zh: <div>
+									<div className="paragraph">
+										我无法保证每次更新都能兼容已有的战斗记录文件。如果你的战斗记录无法被正常导入但你不清楚原因，可以联系我，我会尽力帮一起看。
+									</div>
+									<div className="paragraph">
+										如果浏览器缓存因不明原因出问题（比如预设了刚打完绝望满蓝这样的“非法状态”），可尝试用以下方法重置浏览器缓存：<br/>
+										用以下链接进入本工具：<b>{THIS_DOMAIN + "/#/{command}"}</b>，然后把<b>{"{command}"}</b>替换成以下两个指令之一：
+										<ul>
+											<li style={smallGap}><b>resetResourceOverrides</b>: 删除当前时间线上的所有资源预设和技能
+											</li>
+											<li style={smallGap}><b>resetAll</b>: 删除所有本工具相关的浏览器缓存</li>
+										</ul>
+									</div>
+								</div>,
+								ja: <div>
+									<div className="paragraph">
+										後方互換性があるとは限りません。保存したファイルが正しく読み込まれず、その理由がわからない場合はお問い合わせください。できる限りお手伝いします。
+									</div>
+									<div className="paragraph">
+										ブラウザのキャッシュが何らかの理由で壊れている場合、次の方法でリセットできます。<br/>
+										<b>{THIS_DOMAIN + "/#/{command}"}</b> にアクセスし、<b>{"{command}"}</b> を以下のいずれかに置き換えます：
+										<ul>
+											<li style={smallGap}><b>resetResourceOverrides</b>:
+												全てのリソースとタイムライン上のアクションを上書きします。
+											</li>
+											<li style={smallGap}><b>resetAll</b>: 全てのブラウザキャッシュを削除します。
+											</li>
+										</ul>
+									</div>
+								</div>,
+							})}
+							<div>
+								Or just: <button style={{color: "#be0f0f"}} onClick={() => {
+									clearCachedValues();
+									window.location.reload();
+								}}>{localize({
+									en: "[DANGER!] clear browser cache and reload",
+									zh: "[谨慎操作] 清除缓存并刷新",
+									ja: "[キケン！] ブラウザキャッシュをクリアしてリロード",
+								})}</button>
 							</div>
-						</div>,
-						zh: <div>
-							<div className="paragraph">
-								我无法保证每次更新都能兼容已有的战斗记录文件。如果你的战斗记录无法被正常导入但你不清楚原因，可以联系我，我会尽力帮一起看。
-							</div>
-							<div className="paragraph">
-								如果浏览器缓存因不明原因出问题（比如预设了刚打完绝望满蓝这样的“非法状态”），可尝试用以下方法重置浏览器缓存：<br/>
-								用以下链接进入本工具：<b>{THIS_DOMAIN + "/#/{command}"}</b>，然后把<b>{"{command}"}</b>替换成以下两个指令之一：
-								<ul>
-									<li style={smallGap}><b>resetResourceOverrides</b>: 删除当前时间线上的所有资源预设和技能</li>
-									<li style={smallGap}><b>resetAll</b>: 删除所有本工具相关的浏览器缓存</li>
-								</ul>
-							</div>
-						</div>,
-						ja: <div>
-								<div className="paragraph">
-									後方互換性があるとは限りません。保存したファイルが正しく読み込まれず、その理由がわからない場合はお問い合わせください。できる限りお手伝いします。
-								</div>
-								<div className="paragraph">
-									ブラウザのキャッシュが何らかの理由で壊れている場合、次の方法でリセットできます。<br/>
-									<b>{THIS_DOMAIN + "/#/{command}"}</b> にアクセスし、<b>{"{command}"}</b> を以下のいずれかに置き換えます：
-								<ul>
-									<li style={smallGap}><b>resetResourceOverrides</b>: 全てのリソースとタイムライン上のアクションを上書きします。</li>
-									<li style={smallGap}><b>resetAll</b>: 全てのブラウザキャッシュを削除します。</li>
-								</ul>
-							</div>
-						</div>,
-						})}/>
+						</>
+					}/>
 				</div>
 			</div>}
 		/>
@@ -255,149 +279,7 @@ export function IntroSection(props: {
 						</>}
 					</ul>,
 				})}
-				{job === ShellJob.BLM &&
-				<div className="paragraph"><Expandable title={"Implementation notes"} titleNode={localize({en: "Implementation notes", zh: "实现细节", ja: "実装に関するメモ"})} defaultShow={false} content={
-					<div>
-						{localize({
-							en:
-								<div className="paragraph">
-									[DT] Ice spells seem to grant MP when they land on target(s), aka skillApplicationDelay seconds after snapshot. See <a href={"https://drive.google.com/file/d/1fnjBoXlbIQR1StFlfrwH6eQYE90rV1V5/view?usp=drive_link"}>this screen recording</a>.
-									Note that Umbral Soul also has ~0.633s skill application delay.
-								</div>,
-							zh:
-								<div className="paragraph">
-									[7.0] 从录屏（<a href={"https://drive.google.com/file/d/1fnjBoXlbIQR1StFlfrwH6eQYE90rV1V5/view?usp=drive_link"}>Google Drive</a>）来看，冰技能是在伤害判定的瞬间回蓝。顺便，灵极魂也有0.633秒左右的技能生效延迟，按下技能后需要等待相应时间才有回蓝。
-								</div>
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									[DT] Manafont now has a much shorter (~0.2s) skill application delay compared to in EW. In fact only MP regen from Manafont has this delay; all other effects seem instant.
-								</div>,
-							zh:
-								<div className="paragraph">
-									[7.0] 魔泉的生效延迟跟6.0相比缩短了很多，目前只有0.2秒左右。而且只有回蓝受这个延迟影响，其它效果都是瞬发。
-								</div>
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									[DT] I re-measured some buff timings from <a href={"https://drive.google.com/drive/folders/1Jp6UEGRLlruERpyQ9CuEMXFsOaVixx48?usp=drive_link"}>screen recordings</a>. Lmk if you have something more exact.
-								</div>,
-							zh:
-								<div className="paragraph">
-									[7.0] 我通过录屏（<a href={"https://drive.google.com/drive/folders/1Jp6UEGRLlruERpyQ9CuEMXFsOaVixx48?usp=drive_link"}>Google Drive</a>）的方式重新测量了几个buff的实际持续时长（有的实际时长比技能描述稍长一些）。如果你有更准确的数据，请务必联系我。
-								</div>
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									Galahad found that slidecast window size is linear with respect to cast time. I made a <a href={"https://github.com/miyehn/ffxiv-blm-rotation/tree/main/scripts"}>script</a>, parsed
-									a few logs and confirmed this. Albeit the slope is tiny (~0.02) so I'm just using 0.5s here
-									for simplicity.
-								</div>,
-							zh:
-								<div className="paragraph">
-									根据加拉哈德的一个理论，滑步窗口长度和读条时间呈线性关系；我写了<a href={"https://github.com/miyehn/ffxiv-blm-rotation/tree/main/scripts"}>这个脚本</a>去扒logs记录，证明此理论是基本准确的。由于这个理论的实际影响非常小（斜率约0.02），实际的滑步窗口还是被设置成了恒定的0.5秒。
-							</div>,
-							ja:
-								<div className="paragraph">
-									Galahad によると滑り撃ちの有効時間がキャスト時間に比例するため、私は<a href={"https://github.com/miyehn/ffxiv-blm-rotation/tree/main/scripts"}>スクリプト</a>を作成していくつかのログを解析して確認しました。傾きは~0.02と非常に小さいため単純化して0.5秒としました。</div>,
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									Astral fire / umbral ice refresh happens at slidecast timing (0.5s before cast finishes)
-								</div>,
-							zh:
-								<div className="paragraph">
-									天语状态会在滑步窗口开始时刷新，也就是读条结束前0.5秒。
-								</div>,
-							ja:
-								<div className="paragraph">
-									AFとUBの更新は滑り撃ちが可能な時間と同じタイミング（キャスト終了の0.5秒前）で発生します。
-								</div>,
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									Thanks to Galahad and Blink, skill application delays (see the last function
-									argument <a href={"https://github.com/miyehn/ffxiv-blm-rotation/blob/main/src/Game/Skills.ts#L48"}>here</a>)
-									should be pretty accurate now: looking at the logs, the ones for spells are between "prepare XX" to actual damage,
-									the others from between "casts XX" to whatever the effect is (mostly buff apply/refresh times).
-									Please contact me if you know how to measure the rest of missing data.
-								</div>,
-							zh:
-								<div className="paragraph">
-									感谢Blink和加拉哈德提供的各种技能后摇/生效延迟数据，详见<a href={"https://github.com/miyehn/ffxiv-blm-rotation/blob/main/src/Game/Skills.ts#L48"}>这里最后一个函数变量</a> 。这些通过log来对比技能释放时间/buff生效刷新时间获得的数据，总的来说是比较准确的。欢迎联系我来查漏补缺。
-								</div>,
-							ja:
-								<div className="paragraph">
-									Galahad と Blink のおかげでアクション適用までの遅延時間の計算はかなり正確になりました（<a href={"https://github.com/miyehn/ffxiv-blm-rotation/blob/main/src/Game/Skills.ts#L48"}>こちら</a>の関数の最後の引数を参照）。
-									ログを確認すると、魔法は「prepare XX」から実際のダメージまで、その他の場合は「casts XX」から効果が発生するまでの間です（主にバフの適用/更新時間）。
-									その他のデータの計測方法をご存知の場合はお知らせください。
-								</div>,
-							})}
-						{localize ({
-							en:
-								<div className="paragraph">
-									Lucid dreaming and thunder ticks have independent tick timers. They each have a random time offset relative to
-									MP ticks. See Config section for the current offsets.
-									For lucid dreaming, the earliest first tick time is 0.623s after you press the skill button. It ticks 7
-									times.
-								</div>,
-							zh:
-								<div className="paragraph">
-									跳雷和跳醒梦的时间都是独立计算的，它们和普通跳蓝之间存在一个随机的时间差，这个时间差可以在右侧的设置界面查看。醒梦后的第一跳蓝最早会出现在技能释放后的0.623秒，总计7跳。
-								</div>,
-							ja:
-								<div className="paragraph">
-									ルーシッドドリームとサンダー系のティックは独立したタイマーを持っています。それぞれMPティックとは異なるランダムな時間のオフセットがあります。現在のオフセットについては設定セクションを参照してください。
-									ルーシッドドリームの場合、最初のティックはスキルボタンを押してから0.623秒後に発生し7回ティックします。
-								</div>,
-						})}
-						{localize ({
-							en:
-								<div className="paragraph">
-									7/29/24 cast time precision update: Unlike GCDs with 2 digits of precision, cast times are now updated to use 3 digits of precision, because it matches with the game more closely.
-									Here is an <a href={"https://docs.google.com/spreadsheets/d/18IN-ygAXd2zCR1A3umV3efYcy8xz6O3f/edit?usp=sharing&ouid=104672178485330237849&rtpof=true&sd=true"}>updated full SPS chart</a>. I also <a href={"https://docs.google.com/spreadsheets/d/1d_oPA34tZ-ZOxZSMI6JQDssLiXHUIVv6qmvMTFzoqL8/edit?usp=sharing"}>spammed some spells</a> and the results are in line with this chart.
-									If you would like to learn more, here it is:&nbsp;
-									<a href={"https://docs.google.com/document/d/1zxUd7vFfs24cGteOeCnEsoCzfDxIvKNROIvDUqG-6no/pub"}>writeup by 米露可丝沐希@延夏 et al. translated to English by shanzhe</a>. More resources (in Chinese):&nbsp;
-									<a href={"/ffxiv-blm-rotation/misc/关于读条技能与瞬发技能取整精度的猜想与实验.pdf"}>some tests results and steps to reproduce</a>.
-								</div>,
-							zh:
-								<div className="paragraph">
-									7/29/24 咏唱时间精度更新：不同于精确到小数点后2位的GCD公式，现在咏唱时间的计算公式精确到小数点后3位。经测试，这个精度更吻合游戏内实际的咏唱时间。
-									关于这个新的咏唱时间公式，可以详见米露可丝沐希@延夏等人的<a href={"https://www.bilibili.com/read/cv29822181/?jump_opus=1"}>这篇文章</a>。<a href={"/ffxiv-blm-rotation/misc/关于读条技能与瞬发技能取整精度的猜想与实验.pdf"}>此处</a>有部分测试结果和详细的复现步骤。<br/>
-									附：目前完整的咏速表（<a
-									href={"https://docs.google.com/spreadsheets/d/18IN-ygAXd2zCR1A3umV3efYcy8xz6O3f/edit?usp=sharing&ouid=104672178485330237849&rtpof=true&sd=true"}>Google Sheets</a>）。我个人做了一些测试，logs结果也符合这个改动（<a href={"https://docs.google.com/spreadsheets/d/1d_oPA34tZ-ZOxZSMI6JQDssLiXHUIVv6qmvMTFzoqL8/edit?usp=sharing"}>Google Sheets</a>）。
-								</div>,
-							ja:
-								<div className="paragraph">
-									[outdated (ja)]{/*2024/8/29: 黒魔紋上のキャストとリキャスト時間が誤っていました。現在は単純に0.85倍にしていますが、最大で0.01秒の誤差があります。Vanilla Milksmoothie が正確な値を提案してくれましたが現在は実装する時間がないため、または過去のtxtファイルが壊れる可能性があるため、実装していません。問題がある場合はお知らせください。*/}
-								</div>,
-						})}
-						{localize({
-							en:
-								<div className="paragraph">
-									8/23/24 FPS tax: the previous "caster tax" config is now replaced by 0.1s + FPS tax, which is also explained in the above writeup. <a href={"https://docs.google.com/document/d/1zxUd7vFfs24cGteOeCnEsoCzfDxIvKNROIvDUqG-6no/pub"}>Linking here again</a>.
-								</div>,
-							zh:
-								<div className="paragraph">
-									8/23/24 帧率税：以往的“读条税”设置现已被更精确的 0.1s + 帧率税 取代，更多信息详见上文提到的<a href={"https://www.bilibili.com/read/cv29822181/?jump_opus=1"}>米露可丝沐希@延夏等人的文章</a>。
-								</div>,
-						})}
-					</div>
-
-				}/>
-				</div>
-				}
 				<Changelog/>
-				<Expandable
-					defaultShow={false}
-					title={"Debug"}
-					content={<DebugOptions/>}
-				/>
 			</div>
 			}
 		/>
