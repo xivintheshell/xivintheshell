131268396b695e097cdd0c55be9193cef104e330
Akairyu Gestalter <AkairyuGestalter@users.noreply.github.com>
2024-12-18 00:03:46 -0600

    Deduplicate partybuff icons where jobs are available

--- src/Components/Buffs.tsx
+++ src/Components/Buffs.tsx
@@ -3,30 +3,29 @@ import { updateTimelineView } from "./Timeline";
 
 // grabbed most of them from xivanalysis
 export const buffIcons = new Map();
-buffIcons.set(BuffType.LeyLines, require("./Asset/PartyBuffs/leylines.png"));
-buffIcons.set(BuffType.Tincture, require("./Asset/PartyBuffs/tincture.png"));
-buffIcons.set(BuffType.ArcaneCircle, require("./Asset/PartyBuffs/arcane_circle.png"));
-buffIcons.set(BuffType.ArmysPaeon, require("./Asset/PartyBuffs/armys_paeon.png"));
+buffIcons.set(BuffType.LeyLines, require("./Asset/Buffs/BLM/Ley Lines.png"));
+buffIcons.set(BuffType.Tincture, require("./Asset/Buffs/Role/Tincture.png"));
+buffIcons.set(BuffType.ArcaneCircle, require("./Asset/Buffs/RPR/Arcane Circle.png"));
+buffIcons.set(BuffType.ArmysPaeon, require("./Asset/Buffs/BRD/Army's Paeon.png"));
 buffIcons.set(BuffType.BattleLitany, require("./Asset/PartyBuffs/battle_litany.png"));
-buffIcons.set(BuffType.BattleVoice, require("./Asset/PartyBuffs/battle_voice.png"));
+buffIcons.set(BuffType.BattleVoice, require("./Asset/Buffs/BRD/Battle Voice.png"));
 buffIcons.set(BuffType.Brotherhood, require("./Asset/PartyBuffs/brotherhood.png"));
 buffIcons.set(BuffType.Card_TheBalance, require("./Asset/PartyBuffs/the_balance.png"));
 buffIcons.set(BuffType.Card_TheSpear, require("./Asset/PartyBuffs/the_spear.png"));
 buffIcons.set(BuffType.ChainStratagem, require("./Asset/PartyBuffs/chain_stratagem.png"));
-// I have no idea why "./Asset/PartyBuffs/devilment.png" crashes..:
 buffIcons.set(BuffType.Devilment, require("./Asset/Buffs/DNC/Devilment.png"));
 buffIcons.set(BuffType.Divination, require("./Asset/PartyBuffs/divination.png"));
 buffIcons.set(BuffType.Dokumori, require("./Asset/PartyBuffs/dokumori.png"));
-buffIcons.set(BuffType.Embolden, require("./Asset/PartyBuffs/embolden.png"));
-buffIcons.set(BuffType.MagesBallad, require("./Asset/PartyBuffs/mages_ballad.png"));
-buffIcons.set(BuffType.RadiantFinale1, require("./Asset/PartyBuffs/radiant_finale.png"));
-buffIcons.set(BuffType.RadiantFinale2, require("./Asset/PartyBuffs/radiant_finale.png"));
-buffIcons.set(BuffType.RadiantFinale3, require("./Asset/PartyBuffs/radiant_finale.png"));
+buffIcons.set(BuffType.Embolden, require("./Asset/PartyBuffs/Embolden.png")); // Everyone else sees the sword icon embolden
+buffIcons.set(BuffType.MagesBallad, require("./Asset/Buffs/BRD/Mage's Ballad.png"));
+buffIcons.set(BuffType.RadiantFinale1, require("./Asset/Buffs/BRD/Radiant Finale.png"));
+buffIcons.set(BuffType.RadiantFinale2, require("./Asset/Buffs/BRD/Radiant Finale.png"));
+buffIcons.set(BuffType.RadiantFinale3, require("./Asset/Buffs/BRD/Radiant Finale.png"));
 buffIcons.set(BuffType.SearingLight, require("./Asset/PartyBuffs/searing_light.png"));
-buffIcons.set(BuffType.StandardFinish, require("./Asset/PartyBuffs/standard_step.png"));
-buffIcons.set(BuffType.StarryMuse, require("./Asset/PartyBuffs/starry_muse.png"));
-buffIcons.set(BuffType.TechnicalFinish, require("./Asset/PartyBuffs/technical_step.png"));
-buffIcons.set(BuffType.WanderersMinuet, require("./Asset/PartyBuffs/wanderers_minuet.png"));
+buffIcons.set(BuffType.StandardFinish, require("./Asset/Buffs/DNC/Standard Finish.png"));
+buffIcons.set(BuffType.StarryMuse, require("./Asset/Buffs/PCT/Starry Muse.png"));
+buffIcons.set(BuffType.TechnicalFinish, require("./Asset/Buffs/DNC/Technical Finish.png"));
+buffIcons.set(BuffType.WanderersMinuet, require("./Asset/Buffs/BRD/Wanderer's Minuet.png"));
 
 buffIcons.set(BuffType.Manafication, require("./Asset/PartyBuffs/manafication.png"));
 buffIcons.set(BuffType.Acceleration, require("./Asset/Buffs/RDM/Acceleration.png"));
@@ -37,6 +36,8 @@ buffIcons.set(BuffType.EnhancedEnpi, require("./Asset/Buffs/SAM/Enhanced Enpi.pn
 
 buffIcons.set(BuffType.DeathsDesign, require("./Asset/Buffs/RPR/Death's Design.png"));
 buffIcons.set(BuffType.ArcaneCircle, require("./Asset/Buffs/RPR/Arcane Circle.png"));
+
+buffIcons.set(BuffType.RagingStrikes, require("./Asset/Buffs/BRD/Raging Strikes.png"));
 
 export const buffIconImages: Map<BuffType, HTMLImageElement> = new Map();
 buffIcons.forEach((path, skillName) => {
