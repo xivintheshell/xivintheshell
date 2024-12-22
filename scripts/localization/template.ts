231c38942103cfc1619c4a8bffa879c1e9b66ed3
miyehn <rainduym@gmail.com>
2024-12-23 03:03:40 +0800

    rebase and DNC wip

--- scripts/localization/template.ts
+++ scripts/localization/template.ts
@@ -2,11 +2,11 @@
 // npx ts-node template.ts
 
 import fs from "node:fs"
-import {MCHResourceType, MCHSkillName} from "../../src/Game/Constants/MCH";
+import {DNCResourceType, DNCSkillName} from "../../src/Game/Constants/DNC";
 
 fs.readFile("skills.txt", "utf8", (err, data) => {
 	let zh = data.split('\n').map(line => line.trim()).filter(l => l.length > 0);
-	let en = Object.keys(MCHSkillName);
+	let en = Object.keys(DNCSkillName);
 	if (zh.length === en.length) {
 		console.log(en.map((_, i) => `[SkillName.${en[i]}, "${zh[i]}"],`).join('\n'));
 	} else {
@@ -16,7 +16,7 @@ fs.readFile("skills.txt", "utf8", (err, data) => {
 
 fs.readFile("resources.txt", "utf8", (err, data) => {
 	let zh = data.split('\n').map(line => line.trim()).filter(l => l.length > 0);
-	let en = Object.keys(MCHResourceType);
+	let en = Object.keys(DNCResourceType);
 	if (zh.length === en.length) {
 		console.log(en.map((_, i) => `[ResourceType.${en[i]}, "${zh[i]}"],`).join('\n'));
 	} else {
