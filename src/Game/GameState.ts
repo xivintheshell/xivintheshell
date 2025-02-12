d2be02265e14f1392d57c1789837a904a5fe40e5
Shanzhe Qi <github@zqsz.me>
2025-02-11 22:09:49 -0800

    remove extra console.log statements

--- src/Game/GameState.ts
+++ src/Game/GameState.ts
@@ -1042,8 +1042,6 @@ export class GameState {
 				falloff: 0, // assume all DoTs have no falloff
 			});
 			pDot.modifiers = [...mods, ...(props?.modifiers ?? [])];
-			console.log(props, "modifiers:");
-			console.log(pDot.modifiers);
 			props.node.addDoTPotency(pDot, props.dotName);
 		}
 	}
