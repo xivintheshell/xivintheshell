d59dc285abdfba2e3743846a6df4ebfbbf1020b2
miyehn <miyehn@users.noreply.github.com>
2025-02-26 20:05:56 -0800

    Corrected gcd formula (#112)

--- scripts/sps-LL/test.js
+++ scripts/sps-LL/test.js
@@ -7,25 +7,23 @@
 let levelParam0 = 420;
 let levelParam1 = 2780;
 
-// GCD formula, 2 decimals
+// GCD formula, 2 decimals, outdated (can be off by 0.01s under LL)
 // from: https://bbs.nga.cn/read.php?tid=29774994&rand=903
-function computeGCD(spellSpeed, baseTime, LL) {
-	let subtractLL = LL ? 15 : 0;
-	return Math.floor(Math.floor(Math.floor((100-subtractLL)*100/100)*Math.floor((2000-Math.floor(130*(spellSpeed-levelParam0)/levelParam1+1000))*(1000*baseTime)/10000)/100)*100/100)/100;
+function computeGCD(spellSpeed, baseTime, speedModifier) {
+	return Math.floor(Math.floor(Math.floor((100-speedModifier)*100/100)*Math.floor((2000-Math.floor(130*(spellSpeed-levelParam0)/levelParam1+1000))*(1000*baseTime)/10000)/100)*100/100)/100;
 }
 
 // cast time formula, 3 decimals
-function computeCastTime(spellSpeed, baseTime, LL) {
+function computeCastTime(spellSpeed, baseTime, speedModifier) {
 	let subtractLL = LL ? 15 : 0;
 	return Math.floor(Math.floor(Math.floor((100-subtractLL)*100/100)*Math.floor((2000-Math.floor(130*(spellSpeed-levelParam0)/levelParam1+1000))*(1000*baseTime)/1000)/100)*100/100)/1000;
 }
 
-// old formula, from EW implementation
-function adjustedCastTime(spellSpeed, baseRecastTime, LL) {
+// old formula, from EW implementation but turns out is more accurate..
+function allaganGcd(spellSpeed, baseTime, speedModifier) {
 	let ceil = Math.ceil((levelParam0 - spellSpeed) * 130 / levelParam1);
-	let pts = Math.floor(baseRecastTime * (1000 + ceil));
-	let llFactor = LL ? 0.85 : 1;
-	return Math.floor(llFactor * pts / 10) / 100;
+	let pts = Math.floor(baseTime * (1000 + ceil));
+	return Math.floor((100 - speedModifier) * pts / 1000) / 100;
 }
 
 let baseRecastTimes = [1.5, 2.0, 2.5, 2.8, 3.0, 3.5, 4.0];
@@ -34,19 +32,11 @@ let LL = false;
 let allTestsPassed = true;
 baseRecastTimes.forEach(baseCastTime => {
 	for (let sps = 400; sps < 3000; sps++) {
-		let s1 = computeGCD(sps, baseCastTime, LL);
-		/*
-		// the old formula is the same as new when there's no LL, but LL makes it fail.
-		let s2 = adjustedCastTime(sps, baseCastTime, LL);
-		if (s1 !== s2) {
-			console.log(`${sps} (${baseCastTime}): ${s1} != ${s2}`);
-			allTestsPassed = false;
-		}
-		 */
-		let s3 = computeCastTime(sps, baseCastTime, LL);
-		let diff = s3 - s1;
-		if (diff < 0 || diff >= 0.01) {
-			console.log("!");
+		let s1 = computeGCD(sps, baseCastTime, LL ? 15 : 0);
+		let s2 = allaganGcd(sps, baseCastTime, LL ? 15 : 0);
+		let diff = s2 - s1;
+		if (Math.abs(diff) >= 0.001) {
+			console.log(`sps: ${sps}, baseCastTime: ${baseCastTime}, old: ${s1}, allagan: ${s2}`);
 			allTestsPassed = false;
 		}
 	}
