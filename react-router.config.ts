c3a77e1a8c185b386ad7d57bae7c6423c97ab404
zqsz-xiv <github@zqsz.me>
2025-05-12 00:49:58 -0700

    remove remaining react-router config

--- react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-	appDirectory: "src",
-	ssr: false,
-	// pre-rendering is not possible because of many localStorage accesses
-} satisfies Config;
