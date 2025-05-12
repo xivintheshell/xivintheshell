794e6bbcf6b476b1b03be698a68f976a7e8991e1
zqsz-xiv <github@zqsz.me>
2025-05-12 00:26:10 -0700

    remove react-router

--- src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-
-export default [
-	// * matches all URLs, the ? makes it optional so it will match / as well
-	route("*?", "catchall.tsx"),
-] satisfies RouteConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { reactRouter } from "@react-router/dev/vite";
+// import Inspect from "vite-plugin-inspect";
+import react from '@vitejs/plugin-react-swc';
+
 // @ts-expect-error
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	server: {
 		port: 3000,
+		// Resolve ts and tsx files first in the import order, since the transpiler
+		// looks for every single possible extension when resolving imports.
+		// This has a VERY significant performance impact on dev server load time.
+		// https://vite.dev/guide/performance#reduce-resolve-operations
+		resolve: {
+			extensions: ["ts", "tsx", "json", "js", "jsx", "mjs", "mts"],
+		},
+	},
+	build: {
+		outDir: "build",
 	},
 	// Compatibility with migration from jest
 	test: {
@@ -12,6 +24,12 @@ export default defineConfig({
 		environment: "jsdom",
 		setupFiles: ["./vitest.setup.ts"],
 	},
-	// https://github.com/remix-run/react-router/discussions/12655
-	plugins: [!process.env.VITEST && reactRouter()],
+	plugins: [
+		react(),
+		// https://github.com/antfu-collective/vite-plugin-inspect
+		// Uncomment this line + corresponding import if there are perf issues
+		// in the dev build (`npm start`) that are not apparent in a production
+		// build (`npm preview`).
+		// Inspect(),
+	],
 });
