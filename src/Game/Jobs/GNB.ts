a40a7b0c78dd59d7aa2d43b74c1077d552257171
Shanzhe Qi <github@zqsz.me>
2025-07-11 00:05:35 -0700

    add corpus tester and fix GNB combo

--- src/Game/Jobs/GNB.ts
+++ src/Game/Jobs/GNB.ts
@@ -226,7 +226,7 @@ const makeWeaponskill_GNB = (
 ): Weaponskill<GNBState> => {
 	const onConfirm: EffectFn<GNBState> = combineEffects(params.onConfirm, (state) => {
 		// fix gcd combo state
-		if (name !== "SONIC_BREAK") {
+		if (!["SONIC_BREAK", "BURST_STRIKE", "DOUBLE_DOWN", "LIGHTNING_SHOT"].includes(name)) {
 			state.fixGNBComboState(name);
 		}
 
