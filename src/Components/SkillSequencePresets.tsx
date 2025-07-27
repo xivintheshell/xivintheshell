e7fc8b5f5858fb62369b878fe205af63a00f57aa
Shanzhe Qi <github@zqsz.me>
2025-07-27 11:53:29 -0700

    function-ify more components

--- src/Components/SkillSequencePresets.tsx
+++ src/Components/SkillSequencePresets.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState, useReducer } from "react";
 import {
 	Clickable,
 	Expandable,
@@ -18,43 +18,29 @@ type Fixme = any;
 
 export let updateSkillSequencePresetsView = () => {};
 
-class SaveAsPreset extends React.Component {
-	props: Readonly<{ enabled: boolean }>;
-	state: { filename: string };
-	onChange: (val: string) => void;
-	constructor(props: Readonly<{ enabled: boolean }>) {
-		super(props);
-		this.props = props;
-		this.onChange = (val: string) => {
-			this.setState({ filename: val });
-		};
-
-		this.state = {
-			filename: "(untitled)",
-		};
-	}
-	render() {
-		return <form>
-			<Input
-				style={{ display: "inline-block", marginTop: "10px" }}
-				defaultValue={this.state.filename}
-				description={localize({ en: "name: ", zh: "预设名：" })}
-				width={30}
-				onChange={this.onChange}
-			/>
-			<span> </span>
-			<button
-				type={"submit"}
-				disabled={!this.props.enabled}
-				onClick={(e) => {
-					controller.addSelectionToPreset(this.state.filename);
-					e.preventDefault();
-				}}
-			>
-				{localize({ en: "add selection to preset", zh: "将选中的技能保存为预设" })}
-			</button>
-		</form>;
-	}
+function SaveAsPreset(props: { enabled: boolean }) {
+	const [fileName, setFileName] = useState("(untitled)");
+	const onChange = (val: string) => setFileName(val);
+	return <form>
+		<Input
+			style={{ display: "inline-block", marginTop: "10px" }}
+			defaultValue={fileName}
+			description={localize({ en: "name: ", zh: "预设名：" })}
+			width={30}
+			onChange={onChange}
+		/>
+		<span> </span>
+		<button
+			type={"submit"}
+			disabled={!props.enabled}
+			onClick={(e) => {
+				controller.addSelectionToPreset(fileName);
+				e.preventDefault();
+			}}
+		>
+			{localize({ en: "add selection to preset", zh: "将选中的技能保存为预设" })}
+		</button>
+	</form>;
 }
 
 function PresetLine(props: { line: Line }) {
@@ -109,75 +95,64 @@ function PresetLine(props: { line: Line }) {
 	</div>;
 }
 
-export class SkillSequencePresets extends React.Component {
-	static contextType = ColorThemeContext;
+export function SkillSequencePresets() {
+	const colors = useContext(ColorThemeContext);
+	const [, forceUpdate] = useReducer((x) => x + 1, 0);
+	useEffect(() => {
+		updateSkillSequencePresetsView = () => forceUpdate();
+		return () => {
+			updateSkillSequencePresetsView = () => {};
+		};
+	}, []);
+	const hasSelection =
+		controller && controller.record && controller.record.getFirstSelection() !== undefined;
 
-	constructor(props: Readonly<{}>) {
-		super(props);
-		updateSkillSequencePresetsView = this.unboundUpdatePresetsView.bind(this);
-	}
-	componentWillUnmount() {
-		updateSkillSequencePresetsView = () => {};
-	}
-	unboundUpdatePresetsView() {
-		this.forceUpdate();
-	}
-	render() {
-		const hasSelection =
-			controller && controller.record && controller.record.getFirstSelection() !== undefined;
-
-		// @ts-expect-error: this.context is untyped, and we need this to access the ColorTheme context
-		const bg = getThemeField(this.context, "bgMediumContrast");
-		const content = <div>
-			<button
-				style={{ marginBottom: 10 }}
-				onClick={() => {
-					controller.deleteAllLines();
-				}}
-			>
-				{localize({ en: "clear all presets", zh: "重置所有预设" })}
-			</button>
-			<LoadJsonFromFileOrUrl
-				allowLoadFromUrl={false} // disabled for now, until when/if people have some common openers
-				defaultLoadUrl={"https://miyehn.me/ffxiv-blm-rotation/presets/lines/default.txt"}
-				onLoadFn={(content: Fixme) => {
-					if (content.fileType === FileType.SkillSequencePresets) {
-						controller.appendFilePresets(content);
-					} else {
-						window.alert("incorrect file type '" + content.fileType + "'");
-					}
-				}}
-			/>
-			<div
-				style={{
-					outline: "1px solid " + bg,
-					margin: "10px 0",
-					padding: "10px",
-				}}
-			>
-				{controller.getPresetLines().map((line, i) => {
-					return <PresetLine line={line} key={i} />;
-				})}
-				<SaveAsPreset enabled={hasSelection} />
-				<div style={{ marginTop: 16 }}>
-					<SaveToFile
-						fileFormat={FileFormat.Json}
-						getContentFn={() => {
-							return controller.serializedPresets();
-						}}
-						filename={"presets"}
-						displayName={localize({
-							en: "download presets to file",
-							zh: "将当前预设下载为文件",
-						})}
-					/>
-				</div>
+	const bg = getThemeField(colors, "bgMediumContrast");
+	const content = <div>
+		<button
+			style={{ marginBottom: 10 }}
+			onClick={() => {
+				controller.deleteAllLines();
+			}}
+		>
+			{localize({ en: "clear all presets", zh: "重置所有预设" })}
+		</button>
+		<LoadJsonFromFileOrUrl
+			allowLoadFromUrl={false} // disabled for now, until when/if people have some common openers
+			defaultLoadUrl={"https://miyehn.me/ffxiv-blm-rotation/presets/lines/default.txt"}
+			onLoadFn={(content: Fixme) => {
+				if (content.fileType === FileType.SkillSequencePresets) {
+					controller.appendFilePresets(content);
+				} else {
+					window.alert("incorrect file type '" + content.fileType + "'");
+				}
+			}}
+		/>
+		<div
+			style={{
+				outline: "1px solid " + bg,
+				margin: "10px 0",
+				padding: "10px",
+			}}
+		>
+			{controller.getPresetLines().map((line, i) => <PresetLine line={line} key={i} />)}
+			<SaveAsPreset enabled={hasSelection} />
+			<div style={{ marginTop: 16 }}>
+				<SaveToFile
+					fileFormat={FileFormat.Json}
+					getContentFn={() => controller.serializedPresets()}
+					filename={"presets"}
+					displayName={localize({
+						en: "download presets to file",
+						zh: "将当前预设下载为文件",
+					})}
+				/>
 			</div>
-		</div>;
-		return <Expandable
-			title={localize({ en: "Skill sequence presets", zh: "技能序列预设" }) as string}
-			content={content}
-			defaultShow={false}
-		/>;
-	}
+		</div>
+	</div>;
+	return <Expandable
+		title={localize({ en: "Skill sequence presets", zh: "技能序列预设" }) as string}
+		content={content}
+		defaultShow={false}
+	/>;
 }
